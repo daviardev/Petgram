fe8d7f2f28411e46a3cb4300cfd33a4ef45be8d8
jersonsilvaa <jerson.silva99@hotmail.com>
2022-05-23 19:07:36 -0500

    Creando custom hook para useLocalStorage y useNearScreen

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,46 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import { ImgWrapper, Img, Button, Article } from "./styles";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
-const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-    const element = useRef(null)
-    const [show, setShow] = useState(false)
-    const key = `like${id}`
-    const [liked, setLiked] = useState(()=> {
-        try {
-            const like = window.localStorage.getItem(key)
-            return like
-        } catch (e) {
-            return false
-        }
-    })
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useNearScreen } from "../../hooks/useNearScreen";
 
-    useEffect(()=>{
-        Promise.resolve(
-            typeof window.IntersectionObserver !== 'undefined'
-            ? window.IntersectionObserver
-            : import('intersection-observer')
-        ).then(()=>{
-            const observer = new window.IntersectionObserver((entries)=>{
-                const { isIntersecting } = entries[0]
-                if (isIntersecting) {
-                    setShow(true)
-                    observer.disconnect()
-                }
-            })
-            observer.observe(element.current)
-        })
-    }, [element])
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+    const [show, element] = useNearScreen()
+    const key = `like${id}`
+    const [liked, setLiked] = useLocalStorage(key, false)
 
     const Icon = liked ? FaHeart : FaRegHeart
-    const setLocalStorage = value => {
-        try {
-            window.localStorage.setItem(key, value)
-            setLiked(value)
-        } catch (e) {
-            console.error(e)
-        }
-    }
     
     return (
         <Article ref={element}>
@@ -51,7 +22,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                     <Img src={src} /> 
                 </ImgWrapper>
             </a>
-            <Button onClick={()=> setLocalStorage(!liked)}>
+            <Button onClick={()=> setLiked(!liked)}>
                 <Icon size='22px'/> {likes} likes!</Button>
             </>
             }
